b3b21acf7381a74049b79f9a05bc0d11dc2be177
Анатолій <t0663261690@gmail.com>
2024-04-18 12:03:08 +0300

    the run

--- main.cpp
+++ main.cpp
@@ -74,13 +74,14 @@ void saveEmployeesToFile(const vector<Employee>& employees, const string& filena
     cout << "Employee data saved to file." << endl;
 }
 
-vector<Employee> loadEmployeesFromFile(const string& filename) {
-    vector<Employee> employees;
+void loadEmployeesFromFile(vector<Employee>& employees, const string& filename) {
     ifstream inputFile(filename);
     if (!inputFile) {
         cerr << "Error opening file for reading." << endl;
-        return employees;
+        return;
     }
+
+    employees.clear();
 
     Employee emp;
     while (inputFile >> emp.surname >> emp.name >> emp.age) {
@@ -89,48 +90,74 @@ vector<Employee> loadEmployeesFromFile(const string& filename) {
 
     inputFile.close();
     cout << "Employee data loaded from file." << endl;
-    return employees;
 }
 
+int x = -1;
 int main() {
-    //system("chcp 1251>nul");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     vector<Employee> employees;
 
-    
-    employees.push_back(enterEmployeeData());
+    while (x != 0) {
+        system("cls");
+        cout << "#---------------------------------------------#" << endl;
+        cout << "#                    Меню                     #" << endl;
+        cout << "#                Зчитати з файлу     - 1      #" << endl;
+        cout << "#          Занести нового працівника - 2      #" << endl;
+        cout << "#               Збереження у файл    - 3      #" << endl;
+        cout << "#                Пошук за датой      - 4      #" << endl;
+        cout << "#              Пошук за Прізвищем    - 5      #" << endl;
+        cout << "#           Пошук за часткою Прізвища- 6      #" << endl;
+        cout << "#                    Вихід           - 0      #" << endl;
+        cout << "#---------------------------------------------#" << endl;
+        cin >> x;
+        if (x == 1) {
+            loadEmployeesFromFile(employees, "employees.txt");
+        }
+        if (x == 2) {
+            employees.push_back(enterEmployeeData());
+        }
+        if (x == 3) {
+            saveEmployeesToFile(employees, "employees.txt");
+        }
+        if (x == 4) {
+            int searchAge;
+            cout << "Введіть скільки років для пошуку: ";
+            cin >> searchAge;
+            vector<Employee> employeesByAge = findEmployeesByAge(employees, searchAge);
+            cout << "Вік " << searchAge << ":" << endl;
+            for (const auto& emp : employeesByAge) {
+                printEmployee(emp);
+            }
+        }
+        if (x == 5) {
+            string searchSurname;
+            cout << "Введить повне призвище для пошуку: ";
+            cin >> searchSurname;
+            vector<Employee> employeesBySurname = findEmployeesBySurname(employees, searchSurname);
+            cout << "Призвище " << searchSurname << ":" << endl;
+            for (const auto& emp : employeesBySurname) {
+                printEmployee(emp);
+            }
+        }
+        if (x == 6) {
+            string searchPartialSurname;
+            cout << "Введіть частку призвища для пошуку: ";
+            cin >> searchPartialSurname;
+            vector<Employee> employeesByPartialSurname = findEmployeesByPartialSurname(employees, searchPartialSurname);
+            cout << "Призвище " << searchPartialSurname << ":" << endl;
+            for (const auto& emp : employeesByPartialSurname) {
+                printEmployee(emp);
+            }
+        }
 
-    saveEmployeesToFile(employees, "employees.txt");
-
-    int searchAge;
-    cout << "Введіть скільки років для пошуку: ";
-    cin >> searchAge;
-    vector<Employee> employeesByAge = findEmployeesByAge(employees, searchAge);
-    cout << "Вік " << searchAge << ":" << endl;
-    for (const auto& emp : employeesByAge) {
-        printEmployee(emp);
+        if (x == 0) {
+            cout << "До побачення." << endl;
+            return 0;
+        }
+        system("pause");
     }
-
     
-    string searchSurname;
-    cout << "Введить повне призвище для пошуку: ";
-    cin >> searchSurname;
-    vector<Employee> employeesBySurname = findEmployeesBySurname(employees, searchSurname);
-    cout << "Призвище " << searchSurname << ":" << endl;
-    for (const auto& emp : employeesBySurname) {
-        printEmployee(emp);
-    }
-
-    
-    string searchPartialSurname;
-    cout << "Введіть частку призвища для пошуку: ";
-    cin >> searchPartialSurname;
-    vector<Employee> employeesByPartialSurname = findEmployeesByPartialSurname(employees, searchPartialSurname);
-    cout << "Призвище " << searchPartialSurname << ":" << endl;
-    for (const auto& emp : employeesByPartialSurname) {
-        printEmployee(emp);
-    }
 
     return 0;
 }
